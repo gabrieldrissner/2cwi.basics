4f83b8fef35784466c2fe6f4ffc1b5a652d64d62
Gabriel <gabriel.drissner@student.htldornbirn.at>
2022-01-20 14:31:41 +0100

    test2cwi

---
+++ 2/script.js
@@ -0,0 +1,13 @@
+console.log("Ich zähle von 1 bis 8")
+for (let count = 1;count<=8;count++){
+    console.log(count);
+}
+console.log("Das wars!")
+
+console.log(" ")
+
+console.log("Ich zähle in Zweier-Schritten Rückwärts.")
+
+for (let number = 11; number >=-3;number = number -2){
+    console.log(number)
+}
